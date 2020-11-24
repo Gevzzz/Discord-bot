35f39f2cda646ec6a4a8eb70c64b9b933f6e8b45
Gevorg Minasyan <55835135+Gevzzz@users.noreply.github.com>
2020-11-25 00:35:50 +0100

    Add files via upload

---
+++ CommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Addons.Interactive;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bot
+{
+   public class CommandHandler
+    {
+        private DiscordSocketClient _client;
+
+        private CommandService _service;
+
+        private IServiceProvider _services;
+
+        public CommandHandler(DiscordSocketClient client)
+        {
+            _client = client;
+
+            _service = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton<InteractiveService>()
+                .BuildServiceProvider();
+
+            _service.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+            if (msg == null) return;
+
+            var context = new SocketCommandContext(_client, msg);
+
+            var argPos = 0;
+            var prefix = '&';
+            if (msg.HasCharPrefix(prefix, ref argPos))
+            {
+                var result = await _service.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                    await context.Channel.SendMessageAsync(result.ErrorReason);
+                }
+            }
+        }
+
+    }
+   
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+
+namespace Bot
+{
+    public class Program
+    {
+        //invite link https://discord.com/oauth2/authorize?client_id=779682049865809951&permissions=8&scope=bot
+        static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult();
+
+       
+        private DiscordSocketClient _client;
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Starting bot...");
+
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info
+            });
+
+            _client.Log += Log;
+
+            
+
+            
+
+            await _client.LoginAsync(TokenType.Bot, "Nzc5NjgyMDQ5ODY1ODA5OTUx.X7kFfg.oIKbwQTDXb3dBAv-JEqkh5TbC2Q");
+            await _client.StartAsync();
+
+            _handler = new CommandHandler(_client);
+
+            while (_client.ConnectionState.ToString() != "Connected") { }
+            await Task.Delay(700);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Bot connected!");
+
+            while (_client.ConnectionState.ToString() == "Connected")
+            {
+                var delay = Task.Delay(60000);
+                Console.WriteLine($"\nClient latency: {_client.Latency}ms");
+                await delay;
+            }
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
+
